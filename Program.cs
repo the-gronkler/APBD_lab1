f682e5ad0772e30a76d828eb7ed4ac983c49573a
the-gronkler <alexsvirin1337@gmail.com>
2024-03-18 18:36:48 +0100

    created Calc class with getAverage method

--- Program.cs
+++ Program.cs
@@ -5,3 +5,19 @@
 
 // modification 3
 Console.WriteLine("Hello, World!");
+
+class Calc
+{
+    public static double getAverage(int[] numbers)
+    {
+        if (numbers.Length == 0)
+            return 0;
+
+        double sum = 0;
+        foreach (var num in numbers)
+        {
+            sum += num;
+        }
+        return sum / numbers.Length;
+    }
+}
