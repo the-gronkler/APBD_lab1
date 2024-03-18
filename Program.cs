c9a1484e4aa46aa4260e1396fb8f02ee771ccb3b
the-gronkler <alexsvirin1337@gmail.com>
2024-03-18 22:20:54 +0100

    added GetMax(int[]) method to Calc

--- Program.cs
+++ Program.cs
@@ -20,4 +20,14 @@ class Calc
         }
         return sum / numbers.Length;
     }
+    public static double GetMax(int[] numbers)
+    {
+        int max = numbers[0];
+        foreach (var num in numbers)
+        {
+            if (num > max)
+                max = num
+        }
+        return max;
+    }
 }
